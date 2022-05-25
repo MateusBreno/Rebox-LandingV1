fa107e31e09161593d793e144922186a0c1c2956
MateusBreno <wilquer.fig@gmail.com>
2022-05-25 19:03:07 -0300

    gifs

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,9 +1,9 @@
 import stenio from '../assets/png/stenioMenorAPB.png';
-import prova2 from '../assets/png/2menor.png';
-import prova3 from '../assets/png/3menor.png';
-import prova5 from '../assets/png/5menor.png';
-import prova6 from '../assets/png/6menor.png';
-import prova10 from '../assets/png/10menor.png';
-import prova11 from '../assets/png/11menor.png';
+import prova2 from '../assets/png/2menor.gif';
+import prova3 from '../assets/png/3menor.gif';
+import prova5 from '../assets/png/5menor.gif';
+import prova6 from '../assets/png/6menor.gif';
+import prova10 from '../assets/png/10menor.gif';
+import prova11 from '../assets/png/11menor.gif';
 
 export default { stenio, prova2, prova3, prova5, prova6, prova10, prova11 };
